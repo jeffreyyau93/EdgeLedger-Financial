71fbb525737f23a75ca27310cc3c46c5f1537a91
Jeffrey Yau <jeffreyyau@outlook.com>
2020-06-14 01:06:42 +0800

    feat: Add navbar fading & smooth scrolling effects

--- js/main.js
+++ js/main.js
@@ -1,29 +1,26 @@
-// Initialize and add the map
-function initMap() {
-  // Your location
-  const loc = { lat: 25.033964, lng: 121.564468 };
-  // Centered map on location
-  const map = new google.maps.Map(document.querySelector(".map"), {
-    zoom: 14,
-    center: loc
-  });
-  // The marker, positioned at location
-  const marker = new google.maps.Marker({ position: loc, map: map });
-}
+const navbar = document.getElementById("navbar");
+let scrolled = false;
 
-// Sticky menu background
-window.addEventListener("scroll", function() {
-  if (this.window.scrollY > 150) {
-    document.querySelector("#navbar").style.opacity = 0.9;
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove("top");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(() => {
+      navbar.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200);
   } else {
-    document.querySelector("#navbar").style.opacity = 1;
+    navbar.classList.add("top");
+    scrolled = false;
   }
-});
+};
 
 // Smooth Scrolling
-$("#navbar a, .btn").on("click", function(event) {
+$("#navbar a, .btn").on("click", function (e) {
   if (this.hash !== "") {
-    event.preventDefault();
+    e.preventDefault();
     const hash = this.hash;
     $("html, body").animate({ scrollTop: $(hash).offset().top - 100 }, 800);
   }
